ceb0190f2c645ba84098bf22f4e48a8902973366
muhammadBilal924 <mbilal60924@gmail.com>
2022-02-15 18:06:13 +0500

    test

---
+++ src/components/if-else.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h1>Conditional Rending in vue js</h1>  
+    </div>
+</template>
+<script>
+export default  {
+      name:'if-condition',
+      props:{
+          data:String,
+          msg:String,
+      },data()
+      {
+          return{show:true}
+      },
+      methods:{
+      display()
+      {
+          this.show=!this.show
+      }
+      }  
+    
+}
+</script>
